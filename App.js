50bd5cf3c21651b79cc816ffc29943c00e86c935
cjoon <cjoon2875@gmail.com>
2021-01-27 22:40:12 +0900

    test chage

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends Component {
   render() {
     return (
       <View>
-        <Text>Hello World!</Text>
+        <Text>Hello World! again</Text>
       </View>
     );
   }
